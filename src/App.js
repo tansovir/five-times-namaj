491b2d8c4ce5fb6c1a64667bb8ea94691adf2781
tanvirnpm <tanvirnpm@gmail.com>
2021-09-03 14:36:47 +0600

    data successfully send to api

--- src/App.js
+++ src/App.js
@@ -10,22 +10,46 @@ import axios from 'axios';
 
 
 function App() {
+  const apiURL = `http://localhost/salah/salah-laravel-rest-api/public/api/salahs`;
   const [salahs, setSalahs] = useState([]);
+  const [newSalahs, setnewSalahs] = useState({
+    fojor: '',
+    juhor: '',
+    asor: '',
+    magrib: '',
+    esha: '',
+    tahajjud: ''
+  });
   useEffect(()=>{
-    const apiURL = 'http://localhost/salah/salah-laravel-rest-api/public/api/salahs';
-    axios.get(apiURL).then((res)=>{
+    loadAPI(apiURL);
+  },[])
+  // load api
+  const loadAPI = url => {
+    axios.get(url).then((res)=>{
       const data = res.data;
       setSalahs(data);
     })
-  },[])
-  const salahSubmitHandler = (e) => {
+  }
+  // set new salah to api
+  const setNewSalahs = e => {
     e.preventDefault();
+    const apiURL = `http://localhost/salah/salah-laravel-rest-api/public/api/salahs`;
+    axios.post(apiURL, newSalahs).then(res=>console.log(res));
+    setnewSalahs({
+      fojor: '',
+      juhor: '',
+      asor: '',
+      magrib: '',
+      esha: '',
+      tahajjud: ''
+    })
   }
-  const salahOnChangeHandler = (e) => {
-    const {name, value} = e.target;
-    console.log(name, ':' , value);
+  // get new salah from user form
+  const getNewSalahs = e => {
+    setnewSalahs({...newSalahs,
+      [e.target.name] : e.target.value
+    })
   }
-
   
   return (
     <div className="full-container">
@@ -33,7 +57,7 @@ function App() {
         <Row>
           <Col md={6} className="mt-5">
             <Alert variant="danger" className="text-center">Are you make your salah?</Alert>
-            <Prayerform salahSubmitHandler={salahSubmitHandler} salahOnChangeHandler={salahOnChangeHandler}/>
+            <Prayerform setNewSalahs={setNewSalahs} getNewSalahs={getNewSalahs}/>
           </Col>
           <Col md={6} className="mt-5">
             <Alert variant="success" className="text-center">Your Amol History</Alert>
--- src/component/Prayerform/Prayerform.js
+++ src/component/Prayerform/Prayerform.js
@@ -3,45 +3,45 @@ import { Form, Button } from 'react-bootstrap';
 
 const Prayerform = (props) => {
     // console.log(props)
-    const {salahOnChangeHandler, salahSubmitHandler} = props;
+    const {getNewSalahs, setNewSalahs} = props;
     return (
-        <Form onSubmit={salahSubmitHandler} className="row mx-1 mx-md-0">
-            <Form.Group onChange={salahOnChangeHandler} controlId="fojor" className='col-6 col-md-4 py-2 border rounded'>
+        <Form onSubmit={setNewSalahs} className="row mx-1 mx-md-0">
+            <Form.Group onChange={getNewSalahs} controlId="fojor" className='col-6 col-md-4 py-2 border rounded'>
                 <Form.Label>Fojor</Form.Label>
                 <Form.Check type="radio" value='No' name="fojor" label="No"/>
                 <Form.Check type="radio" value="Right Time" name="fojor" label="Right Time"/>
                 <Form.Check type="radio" value="Kaja" name="fojor" label="Kaja"/>
                 <Form.Check type="radio" value="Befor Kaja" name="fojor" label="Befor Kaja"/>
             </Form.Group>
-            <Form.Group onChange={salahOnChangeHandler} controlId="juhor" className='col-6 col-md-4 py-2 border rounded'>
+            <Form.Group onChange={getNewSalahs} controlId="juhor" className='col-6 col-md-4 py-2 border rounded'>
                 <Form.Label>juhor</Form.Label>
                 <Form.Check type="radio" value='No' name="juhor" label="No"/>
                 <Form.Check type="radio" value="Right Time" name="juhor" label="Right Time"/>
                 <Form.Check type="radio" value="Kaja" name="juhor" label="Kaja"/>
                 <Form.Check type="radio" value="Befor Kaja" name="juhor" label="Befor Kaja"/>
             </Form.Group>
-            <Form.Group onChange={salahOnChangeHandler} controlId="asor" className='col-6 col-md-4 py-2 border rounded'>
+            <Form.Group onChange={getNewSalahs} controlId="asor" className='col-6 col-md-4 py-2 border rounded'>
                 <Form.Label>asor</Form.Label>
                 <Form.Check type="radio" value='No' name="asor" label="No"/>
                 <Form.Check type="radio" value="Right Time" name="asor" label="Right Time"/>
                 <Form.Check type="radio" value="Kaja" name="asor" label="Kaja"/>
                 <Form.Check type="radio" value="Befor Kaja" name="asor" label="Befor Kaja"/>
             </Form.Group>
-            <Form.Group onChange={salahOnChangeHandler} controlId="magrib" className='col-6 col-md-4 py-2 border rounded'>
+            <Form.Group onChange={getNewSalahs} controlId="magrib" className='col-6 col-md-4 py-2 border rounded'>
                 <Form.Label>magrib</Form.Label>
                 <Form.Check type="radio" value='No' name="magrib" label="No"/>
                 <Form.Check type="radio" value="Right Time" name="magrib" label="Right Time"/>
                 <Form.Check type="radio" value="Kaja" name="magrib" label="Kaja"/>
                 <Form.Check type="radio" value="Befor Kaja" name="magrib" label="Befor Kaja"/>
             </Form.Group>
-            <Form.Group onChange={salahOnChangeHandler} controlId="esha" className='col-6 col-md-4 py-2 border rounded'>
+            <Form.Group onChange={getNewSalahs} controlId="esha" className='col-6 col-md-4 py-2 border rounded'>
                 <Form.Label>esha</Form.Label>
                 <Form.Check type="radio" value='No' name="esha" label="No"/>
                 <Form.Check type="radio" value="Right Time" name="esha" label="Right Time"/>
                 <Form.Check type="radio" value="Kaja" name="esha" label="Kaja"/>
                 <Form.Check type="radio" value="Befor Kaja" name="esha" label="Befor Kaja"/>
             </Form.Group>
-            <Form.Group onChange={salahOnChangeHandler} controlId="tahajjud" className='col-6 col-md-4 py-2 border rounded'>
+            <Form.Group onChange={getNewSalahs} controlId="tahajjud" className='col-6 col-md-4 py-2 border rounded'>
                 <Form.Label>tahajjud</Form.Label>
                 <Form.Check type="radio" value='No' name="tahajjud" label="No"/>
                 <Form.Check type="radio" value="Right Time" name="tahajjud" label="Right Time"/>
